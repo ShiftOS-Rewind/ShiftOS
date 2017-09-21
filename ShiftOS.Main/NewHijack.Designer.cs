cf1bf85e01c8d99f052c09b72d2fb2bbbc0b99b0
FloppyDiskDrive <ibmpcdos5@gmail.com>
2017-09-21 17:16:49 -0500

    Added the SOS code (VB and C#)
    Very, very incomplete (C# isn't *remotely* complete.)

---
+++ ShiftOS.Main/NewHijack.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace HijackScreen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblHijack = new System.Windows.Forms.Label();
+            this.conversationTimer = new System.Windows.Forms.Timer(this.components);
+            this.textGen = new System.Windows.Forms.Timer(this.components);
+            this.lblhackwords = new System.Windows.Forms.Label();
+            this.tmrHackFX = new System.Windows.Forms.Timer(this.components);
+            this.BackgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // lblHijack
+            // 
+            this.lblHijack.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblHijack.AutoSize = true;
+            this.lblHijack.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.lblHijack.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHijack.Location = new System.Drawing.Point(151, 173);
+            this.lblHijack.Name = "lblHijack";
+            this.lblHijack.Size = new System.Drawing.Size(18, 25);
+            this.lblHijack.TabIndex = 2;
+            this.lblHijack.Text = "\\";
+            // 
+            // conversationTimer
+            // 
+            this.conversationTimer.Tick += new System.EventHandler(this.conversationTimer_Tick);
+            // 
+            // textGen
+            // 
+            this.textGen.Interval = 20;
+            this.textGen.Tick += new System.EventHandler(this.textGen_Tick);
+            // 
+            // lblhackwords
+            // 
+            this.lblhackwords.AutoSize = true;
+            this.lblhackwords.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblhackwords.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblhackwords.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.lblhackwords.Location = new System.Drawing.Point(0, 0);
+            this.lblhackwords.Name = "lblhackwords";
+            this.lblhackwords.Size = new System.Drawing.Size(127, 18);
+            this.lblhackwords.TabIndex = 3;
+            this.lblhackwords.Text = "Encrypting MBR...";
+            // 
+            // tmrHackFX
+            // 
+            this.tmrHackFX.Interval = 50;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(653, 457);
+            this.Controls.Add(this.lblHijack);
+            this.Controls.Add(this.lblhackwords);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Label lblHijack;
+        internal System.Windows.Forms.Timer conversationTimer;
+        internal System.Windows.Forms.Timer textGen;
+        internal System.Windows.Forms.Label lblhackwords;
+        internal System.Windows.Forms.Timer tmrHackFX;
+        internal System.ComponentModel.BackgroundWorker BackgroundWorker1;
+    }
+}
+
---
+++ ShiftOS/Paths.vb
@@ -0,0 +1,55 @@
+﻿Module Paths
+
+
+
+    ' Paths:
+    '  Root path - Paths.root - DO NOT USE UNLESS NECISARY (Opening files from the file skimmer, blocking the user from going above C:\ShiftOS, ect)
+    '    Software Data - Paths.progdata - This is the base path for all program data. If you are adding a folder to this path, add a variable in here
+    '      AdvStart - Paths.advda - Stores stuff like recent files, for example if you go to Paths.advstart\Recent in Explorer, you'll see the same files you see in the Advanced AL's Recent section.
+    '      Knowledge Input Data - Paths.kidata - Knowledge Input uses this to save your lists of words you've found
+    '      Shiftnet Data - Paths.shiftnetdata - This is where the shiftnet currently saves it's history
+    '      Download Manager Data - Paths.dnldata - Not sure what this does
+    '    Save Data - Paths.savedata - The game has it's save file in here
+    '      Skin Directory - Paths.skindir - Self explanatory.
+    '        Loaded Skin Directory - Paths.loadedskin - Where the contents of .skn files are extracted to.
+    '          Sound Directory - Paths.sounddir - Where sound files (such as Infobox chime) are stored, coming soon.
+    '        Current Skin Directory - Paths.currentskin - ??????????
+    '    Home Directory - Paths.home - This is the folder that contains all the user's files
+    '      Desktop - Paths.desktop - All the files that appear on your desktop should be stored here
+    '      Documents - Paths.documents - The documents folder is where the user puts their documents
+    '      Downloads - Paths.downloads - The downloads folder is where the downloader downloads downloads from the Shiftnet
+    '      Music - Paths.music - The music folder is where the user puts their music
+    '      Pictures - Paths.pictures - The pictures folder is where the user puts their pictures
+    '      Skins - Paths.skins - The skins folder is where the user puts their skins
+
+
+
+    'Declaration Hierarchy
+
+    Public root As String = "C:\ShiftOS\" 'Do NOT use this unless it is for files that must go DIRECTLY in C:\ShiftOS\
+
+    Public progdata As String = root & "SoftwareData\"
+
+    Public advdata As String = progdata & "AdvStart\"
+    Public kidata As String = progdata & "KnowledgeInput\"
+    Public shiftnetdata As String = progdata & "Shiftnet\"
+    Public dnldata As String = progdata & "DownloadManager\"
+
+    Public savedata As String = root & "Shiftum42\"
+
+    Public skindir As String = savedata & "Skins\"
+
+    Public loadedskin As String = skindir & "Loaded\"
+    Public currentskin As String = skindir & "Current\"
+
+    Public sounddir As String = loadedskin & "Sounds\"
+
+    Public home As String = root & "Home\"
+
+    Public desktop As String = home & "Desktop\"
+    Public documents As String = home & "Documents\"
+    Public downloads As String = home & "Documents\"
+    Public music As String = home & "Music\"
+    Public pictures As String = home & "Pictures\"
+    Public skins As String = home & "Skins\"
+End Module
